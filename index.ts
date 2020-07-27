748caf794d590d2d44c00a4722b79ec1d4534473
cutls <web-pro@cutls.com>
2020-07-27 19:00:41 +0900

    https

--- index.ts
+++ index.ts
@@ -27,7 +27,7 @@ router.get('/get/:domain', async (ctx, next) => {
 	const diff = Jimp.distance(gotimg, compared)
 	let isDefault = false
 	if(diff < 0.21) isDefault = true
-	ctx.body = { success: true, difference: diff, type: type, isDefault: isDefault, url: `${domain}/${file}` }
+	ctx.body = { success: true, difference: diff, type: type, isDefault: isDefault, url: `https://${domain}/${file}` }
 })
 
 koa.use(router.routes())
